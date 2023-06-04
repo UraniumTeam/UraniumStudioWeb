b1f52d72317bbcc92b19e873df38b883f981efcf
n-dub <ragucoz@gmail.com>
2023-06-04 16:11:11 +0500

    Upload sessions to an S3 bucket

--- BackendApi/BackendApi/Controllers/SessionsController.cs
+++ BackendApi/BackendApi/Controllers/SessionsController.cs
@@ -1,4 +1,6 @@
-﻿using BackendApi.Data;
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using BackendApi.Data;
 using BackendApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,12 +12,17 @@ public sealed class SessionsController : ControllerBase
 {
     private readonly ILogger<SessionsController> logger;
     private readonly AppDataContext dataContext;
+    private readonly AmazonS3Client s3Client;
+    private readonly IConfiguration configuration;
     private readonly string sessionDirectory;
 
-    public SessionsController(ILogger<SessionsController> logger, AppDataContext dataContext)
+    public SessionsController(ILogger<SessionsController> logger, AppDataContext dataContext, AmazonS3Client s3Client,
+        IConfiguration configuration)
     {
         this.logger = logger;
         this.dataContext = dataContext;
+        this.s3Client = s3Client;
+        this.configuration = configuration;
 
         sessionDirectory = Path.Join(Directory.GetCurrentDirectory(), "sessions");
         if (!Directory.Exists(sessionDirectory))
@@ -36,9 +43,23 @@ public sealed class SessionsController : ControllerBase
         }
 
         var sessionId = Guid.NewGuid();
-        var filePath = Path.Join(sessionDirectory, sessionId + ".upx");
-        await using var stream = System.IO.File.Create(filePath);
-        await formFile.CopyToAsync(stream);
+        var bucketName = configuration["S3BucketName"];
+
+        var request = new PutObjectRequest
+        {
+            BucketName = bucketName,
+            Key = sessionId.ToString(),
+            InputStream = formFile.OpenReadStream()
+        };
+
+        try
+        {
+            await s3Client.PutObjectAsync(request);
+        }
+        catch (AmazonS3Exception e)
+        {
+            logger.LogError(e, "Error while trying to upload file {} to S3", fileName);
+        }
 
         var metadata = new SessionMetadata
         {
